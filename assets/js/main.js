c0a4aa585b17f357b11b070a343a34787ad2a6f9
AndreSoffiantini <andrea.soffiantini@hotmail.it>
2022-04-07 15:44:45 +0200

    Milestone 2, spacing error in printed strings and personInfo variable not correctly defined

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,7 +30,7 @@ const team = [{
     },
 ];
 
-/* MILESTONE 1 (da fare entro le 18):
+/* MILESTONE 1:
 stampare su console le informazioni di nome, ruolo e la stringa della foto */
 
 /* console.log(team[1]);
@@ -39,9 +39,39 @@ console.log(team[1].name); */
 for (let index in team) {
 
     console.log(`Persona ${Number(index) + 1}`);
-    console.log(team[index].name);
+
+    for (let key in team[index]) {
+
+        console.log(team[index][key]);
+
+    }
+
+    /* console.log(team[index].name);
     console.log(team[index].role);
-    console.log(team[index].image);
+    console.log(team[index].image); */
     console.log(" ");
+}
+
+/* MILESTONE 2:
+stampare le stesse informazioni su DOM sottoforma di stringhe */
+
+for (let index in team) {
+
+    let personSection = document.createElement("div");
+    let personInfo = personSection.innerText;
+
+    document.body.appendChild(personSection);
+
+    personInfo = `Persona ${Number(index) + 1}: `;
+    console.log(personInfo);
+    console.log(personSection.innerText);
+
+    for (let key in team[index]) {
+
+        personSection.innerText += team[index][key] + " ";
+
+    }
+
+    document.body.appendChild(document.createElement("br"));
 
 }
